c60390396aeaacfca1e547e2da9ef847f001b2cd
2014065462 <2014065462@ust-ics.mygbiz.com>
2019-10-05 09:51:41 +0800

    Initial commit

---
+++ Q2_4ITA_Que/app/src/main/java/com/que/q2_4ita_que/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.que.q2_4ita_que;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
